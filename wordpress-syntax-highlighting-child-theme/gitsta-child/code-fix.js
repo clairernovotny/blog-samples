95c880e9d157104420194928ed06010911569435
Oren Novotny <oren@novotny.org>
2016-02-05 11:54:23 -0500

    fix bug

--- wordpress-syntax-highlighting-child-theme/gitsta-child/code-fix.js
+++ wordpress-syntax-highlighting-child-theme/gitsta-child/code-fix.js
@@ -1,10 +1,9 @@
 jQuery(function() {
     $ = jQuery;
-
     $("code").each(function() {
        var className = $(this).attr('class');
-       if(!(className.lastIndexOf('language-', 0) === 0)) {
-            // No language prepend
+       if(className && !(className.lastIndexOf('language-', 0) === 0)) {
+            // No language, prepend
             $(this).attr('class', 'language-' + className);
         }
     });  
